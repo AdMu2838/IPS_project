06b729b38c4031110d7311b8d0b1a96105b1479d
Agustin Mamani <agusdmu1999@gmail.com>
2024-06-04 18:55:15 -0500

    Entidad Empleado

---
+++ src/main/java/com/turismo/venta/entity/Empleado.java
@@ -0,0 +1,45 @@
+package com.turismo.venta.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+
+@Entity
+@Data
+@Table(name = "Empleados")
+public class Empleado {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_empleado")
+    private int idEmpleado;
+
+    @Column(name = "nombre")
+    private String name;
+
+    @Column
+    private String apellido;
+
+    @Column
+    private String direccion;
+
+    @Column
+    private String dni;
+
+    @Column
+    private String fecha_nac;
+
+    @Column
+    private String nacionalidad;
+
+    @Column
+    private String celular;
+
+    @Column
+    private String email;
+
+    @Column
+    private String cargo;
+
+    @Column
+    private Double sueldo;
+}
