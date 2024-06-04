0492229ad575e3c01df5844b54f9b2daa94811a4
Agustin Mamani <agusdmu1999@gmail.com>
2024-06-04 18:53:12 -0500

    Entity Client

---
+++ src/main/java/com/turismo/venta/entity/Client.java
@@ -0,0 +1,40 @@
+package com.turismo.venta.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "Clientes")
+public class Client {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_cliente")
+    private int idClient;
+
+    @Column(name = "nombre")
+    private String name;
+
+    @Column
+    private String apellido;
+
+    @Column
+    private String direccion;
+
+    @Column
+    private String dni;
+
+    @Column
+    private String fecha_nac;
+
+    @Column
+    private String nacionalidad;
+
+    @Column
+    private String celular;
+
+    @Column
+    private String email;
+
+
+}
