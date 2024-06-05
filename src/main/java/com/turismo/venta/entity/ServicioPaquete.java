74aa03c09a4caaaf765368be3fcaa032418404cd
Agustin Mamani <agusdmu1999@gmail.com>
2024-06-04 19:16:40 -0500

    Entidad ServicioPaquete

---
+++ src/main/java/com/turismo/venta/entity/ServicioPaquete.java
@@ -0,0 +1,17 @@
+package com.turismo.venta.entity;
+
+import jakarta.persistence.*;
+
+public class ServicioPaquete {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @ManyToOne
+    @JoinColumn(name = "codigo_paquete", referencedColumnName = "codigo_paquete")
+    private Paquete paquete;
+
+    @ManyToOne
+    @JoinColumn(name = "codigo_servicio", referencedColumnName = "codigo_servicio")
+    private Services servicio;
+}
