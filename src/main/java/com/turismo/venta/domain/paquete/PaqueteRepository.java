4f172e3cb57851298aa6936c782e6cd3d6ba1a99
Agustin Mamani <agusdmu1999@gmail.com>
2024-07-20 14:28:23 -0500

    cambio en paquete

--- src/main/java/com/turismo/venta/domain/paquete/PaqueteRepository.java
+++ src/main/java/com/turismo/venta/domain/paquete/PaqueteRepository.java
@@ -1,7 +1,11 @@
 package com.turismo.venta.domain.paquete;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface PaqueteRepository extends JpaRepository<Paquete, Long> {
-
+    @Query("SELECT p FROM Paquete p WHERE p.paqEstReg = 'A'")
+    Page<Paquete> findActivePackages(Pageable pagination);
 }
--- src/main/java/com/turismo/venta/domain/paquete/PaqueteServicio.java
+++ src/main/java/com/turismo/venta/domain/paquete/PaqueteServicio.java
@@ -1,12 +1,19 @@
 package com.turismo.venta.domain.paquete;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 import java.math.BigDecimal;
 import java.util.List;
 
 public record PaqueteServicio(
+        @NotBlank
         BigDecimal paqCos,
+        @NotBlank
         Character paqEstReg,
+        @NotBlank
         String paqImg,
+        @NotNull
         List<Long> serviciosCodigos
 ) {
 }
