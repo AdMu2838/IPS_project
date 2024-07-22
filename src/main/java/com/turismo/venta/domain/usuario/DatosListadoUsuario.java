7a1795b96b2088ddd08d0192e44194294e956d19
Agustin Mamani <agusdmu1999@gmail.com>
2024-07-22 17:06:51 -0500

    para mandar usuario con rol al listar Co-authored-by: alexisBltz <almamanima@unsa.edu.pe>

--- src/main/java/com/turismo/venta/domain/usuario/DatosListadoUsuario.java
+++ src/main/java/com/turismo/venta/domain/usuario/DatosListadoUsuario.java
@@ -10,10 +10,11 @@ public record DatosListadoUsuario(
         Long id,
         String login,
         Character estadoRegistro,
+        String rol,
         List<DatosListadoDatosUsuario> datosUsuarios
 ) {
     public DatosListadoUsuario(Usuario usuario) {
-        this(usuario.getId(), usuario.getUsuEma(),usuario.getUsuEstReg(),
+        this(usuario.getId(), usuario.getUsuEma(),usuario.getUsuEstReg(), usuario.getUsuRol(),
                 usuario.getDatosUsuarios().stream()
                         .map(DatosListadoDatosUsuario::new)
                         .collect(Collectors.toList()));
